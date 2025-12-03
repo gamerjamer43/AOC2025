880ffc35e538421440763ab18a60cfe762737e55
Noah Mingolelli <gamerjamer43@gmail.com>
2025-12-03 18:20:42 -0500

    starting F#. decided i wanted to do this in 6 languages for some unknown reason lol
    may not get done with all of them but my goal is to do this in a bunch of different paradigms
    
    after this we have 4 more, we'll see how we do

---
+++ Day1/F#/part1.fs
@@ -0,0 +1,19 @@
+﻿(* this is fr my first bout with F#. trying to learn
+from scratch to teach myself functional programming. 
+i found this has a more intermediary feel than ocaml
+so i'll start with that first. also gonna buff out a:
+
+- java solution
+- ocaml solution (if i can get fucking opam on my pc)
+- rust solution
+- C# solution
+- i already did a python but i'll get that over too *)
+
+open System.IO
+
+let filepath: string = "../code.txt"
+let lines: string seq = File.ReadLines filepath
+
+// print with a newline each line in the program
+for line in lines do
+    printfn "%s" line
