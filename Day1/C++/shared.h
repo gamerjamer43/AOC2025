6afd3e9d40978f759bd643ff3442f3a73cdfde6d
Noah Mingolelli <gamerjamer43@gmail.com>
2025-12-06 22:03:18 -0500

    i dont feel like doing the c++ rn i wanna do day 4

---
+++ Day1/C++/shared.h
@@ -0,0 +1,14 @@
+#ifndef SHARED
+#define SHARED
+
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using std::string;
+using std::getline;
+
+using ifs = std::ifstream;
+using ss = std::stringstream;
+
+#endif
